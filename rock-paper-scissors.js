6cf875d5a308a6104d34ae2b4b0b8744e38635e2
Jonathan Tseng <jonathanchristiantseng@gmail.com>
2022-07-24 15:50:06 -0700

    added live running score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,54 +5,57 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log(`PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
+        console.log(`DRAW - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
         content.textContent = `Draw. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
     } else if (playerSelection === "ROCK") {
         if (computerSelection === "PAPER") {
-            console.log(`PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
+            console.log(`LOSE - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You lose. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++computerScore;
         } else {
-            console.log(`PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
+            console.log(`WIN - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You win. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++playerScore;
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection === "ROCK") {
-            console.log(`PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
+            console.log(`WIN - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You win. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++playerScore;
         } else {
-            console.log(`PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
+            console.log(`LOSE - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You lose. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++computerScore;
         }
     } else if (playerSelection === "SCISSOR") {
         if (computerSelection === "PAPER") {
-            console.log(`PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
+            console.log(`WIN - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You win. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++playerScore;
         } else {
-            console.log(`PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
+            console.log(`LOSE - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You lose. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++computerScore;
         }
-    } 
+    }
 }
 
 const rock = document.querySelector('.rock');
 rock.addEventListener('click', () => {
     playRound("ROCK", computerPlay());
+    score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 });
 
 const paper = document.querySelector('.paper');
 paper.addEventListener('click', () => {
     playRound("PAPER", computerPlay());
+    score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 });
 
 const scissor = document.querySelector('.scissor');
 scissor.addEventListener('click', () => {
     playRound("SCISSOR", computerPlay());
+    score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 })
 
 const results = document.querySelector('.results');
@@ -60,6 +63,12 @@ const content = document.createElement('div');
 content.classList.add('content');
 results.appendChild(content);
 
+const score = document.querySelector('.score');
+const scores = document.createElement('div');
+scores.classList.add('scores');
+score.appendChild(scores);
+
 let playerScore = 0;
 let computerScore = 0;
 
+score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
