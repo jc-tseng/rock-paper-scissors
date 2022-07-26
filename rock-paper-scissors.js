bb64d8bd27c484ab85caa33a858956a42812a313
Jonathan Tseng <jonathanchristiantseng@gmail.com>
2022-07-25 21:06:24 -0700

    added computer choice as image instead of just text results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,34 +5,56 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
+        if (playerSelection === "ROCK") {
+            image.src = './imgs/r.png'
+            document.querySelector('.computer').appendChild(image);
+        } else if (playerSelection === "PAPER") {
+            image.src = './imgs/p.png'
+            document.querySelector('.computer').appendChild(image);
+        } else {
+            image.src = './imgs/s.png'
+            document.querySelector('.computer').appendChild(image);
+        }
         console.log(`DRAW - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
         content.textContent = `Draw. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
     } else if (playerSelection === "ROCK") {
         if (computerSelection === "PAPER") {
+            image.src = './imgs/p.png'
+            document.querySelector('.computer').appendChild(image);
             console.log(`LOSE - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You lose. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++computerScore;
         } else {
+            image.src = './imgs/s.png'
+            document.querySelector('.computer').appendChild(image);
             console.log(`WIN - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You win. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++playerScore;
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection === "ROCK") {
+            image.src = './imgs/r.png'
+            document.querySelector('.computer').appendChild(image);
             console.log(`WIN - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You win. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++playerScore;
         } else {
+            image.src = './imgs/s.png'
+            document.querySelector('.computer').appendChild(image);
             console.log(`LOSE - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You lose. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++computerScore;
         }
     } else if (playerSelection === "SCISSOR") {
         if (computerSelection === "PAPER") {
+            image.src = './imgs/p.png'
+            document.querySelector('.computer').appendChild(image);
             console.log(`WIN - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You win. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++playerScore;
         } else {
+            image.src = './imgs/p.png'
+            document.querySelector('.computer').appendChild(image);
             console.log(`LOSE - PLAYER: ${playerSelection} | COMPUTER: ${computerSelection}`)
             content.textContent = `You lose. You chose ${playerSelection} and the computer chose ${computerSelection}.`;
             return ++computerScore;
@@ -86,6 +108,8 @@ const score = document.querySelector('.score');
 const scores = document.createElement('div');
 scores.classList.add('scores');
 score.appendChild(scores);
+
+const image = document.createElement('img');
 
 let playerScore = 0;
 let computerScore = 0;
